329d400d6454740cbcf32032f3615e89f98653f0
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-12-01 19:50:57 +0300

    Update icon in AddEntry.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -4,6 +4,8 @@ import { getMetricMetaInfo, timeToString } from '../utils/helpers'
 import UdaciSlider from './UdaciSlider'
 import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
+import TextButton from './TextButton'
+import { Ionicons } from '@expo/vector-icons'
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -74,9 +76,33 @@ export default class AddEntry extends Component {
 
     }
 
+    reset = () => {
+        const key = timeToString()
+
+        // Update Redux
+
+        // Navigate to Home
+
+        // Save the "DB"
+    }
+
     render () {
         const metaInfo = getMetricMetaInfo()
-        const entry = this.state
+
+        if (this.props.alreadyLogged) {
+            return (
+                <View>
+                    <Ionicons
+                        name='ios-happy'
+                        size={100}
+                    />
+                    <Text>You already logged your information for today</Text>
+                    <TextButton onPress={this.reset} >
+                        Reset
+                    </TextButton>
+                </View>
+            )
+        }
         
         return (
             <View>
