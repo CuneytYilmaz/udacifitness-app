d6008a6ded5acec21d5c12190baf4f828f0dab43
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-12-07 22:03:03 +0300

    Changes for style-metainfo

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
-import CSSinJS from './components/CSSinJS'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
@@ -10,8 +9,8 @@ export default class App extends Component {
   render () {
     return (
       <Provider store={createStore(reducer)} >
-        <View>
-          <CSSinJS />
+        <View style={{flex: 1}}>
+          <AddEntry />
         </View>
       </Provider>
     );
