d791510a205cf040c706eadf366351ce95e60340
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-12-08 00:54:04 +0300

    Changes for style-rest

--- components/UdaciSlider.js
+++ components/UdaciSlider.js
@@ -1,18 +1,35 @@
 import React from 'react'
-import { View, Text, Slider } from 'react-native'
+import { View, Text, Slider, StyleSheet } from 'react-native'
+import { gray } from '../utils/colors'
 
 export default function UdaciSlider ({ value, max, unit, step, onChange }) {
     return (
-        <View>
+        <View style={styles.row}>
             <Slider 
+                style={{flex: 1}}
                 maximumValue={max}
                 minimumValue={0}
                 step={step}
                 value={value}
                 onValueChange={onChange}
             />
-            <Text>{value}</Text>
-            <Text>{unit}</Text>
+            <View style={styles.metricCounter}>
+                <Text style={{fontSize: 24, textAlign: 'center'}}>{value}</Text>
+                <Text style={{fontSize: 18, color: gray}}>{unit}</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    row: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    metricCounter: {
+        width: 85,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
