0e264822e403f034895935f3259313a4e8bac440
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-12-08 21:43:44 +0300

    Changes for finish-navigation

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -20,6 +20,7 @@ import { Ionicons } from '@expo/vector-icons'
 import { connect } from 'react-redux'
 import { addEntry } from '../actions'
 import { white, purple } from '../utils/colors'
+import { NavigationActions } from 'react-navigation'
 
 function SubmitBtn ({ onPress }) {
     return (
@@ -86,7 +87,7 @@ class AddEntry extends Component {
             eat: 0,
         })
 
-        // Navigate to Home
+        this.toHome()
 
         submitEntry({ key, entry })
 
@@ -101,9 +102,15 @@ class AddEntry extends Component {
             [key]: getDailyReminderValue()
         }))
 
-        // Navigate to Home
+        this.toHome()
 
         removeEntry(key)
+    }
+
+    toHome = () => {
+        this.props.navigation.dispatch(NavigationActions.back({
+            key: 'AddEntry'
+        }))
     }
 
     render () {
