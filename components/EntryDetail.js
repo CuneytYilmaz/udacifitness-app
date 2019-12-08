a3c7923118f5ae0779ae4c7d8387e3c37d2c6f52
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-12-08 20:39:45 +0300

    connect-EntryDetail

--- components/EntryDetail.js
+++ components/EntryDetail.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import { connect } from 'react-redux'
+import MetricCard from './MetricCard'
+import { white } from '../utils/colors'
 
 class EntryDetail extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -15,8 +18,11 @@ class EntryDetail extends Component {
     }
 
     render () {
+        const { metrics } = this.props
+
         return (
-            <View>
+            <View style={styles.container} >
+                <MetricCard metrics={metrics} />
                 <Text>
                     Entry Detail - {JSON.stringify(this.props.navigation.state.params.entryId)}
                 </Text>
@@ -25,4 +31,21 @@ class EntryDetail extends Component {
     }
 }
 
-export default EntryDetail
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: white,
+        padding: 15,
+    }
+})
+
+function mapStateToProps (state, { navigation }) {
+    const { entryId } = navigation.state.params
+
+    return {
+        entryId,
+        metrics: state[entryId],
+    }
+}
+
+export default connect(mapStateToProps)(EntryDetail)
